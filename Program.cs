90fd32b21428fc9232952efbe94602f87dddd434
Pagliaraniz <giacomo.pagliarani.stud@ispascalcomandini.it>
2024-11-28 08:15:23 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace applicazione_giornale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
